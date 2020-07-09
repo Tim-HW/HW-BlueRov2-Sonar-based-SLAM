world set up and theta average fixed

src/icp/static/buffers/buffer.py
@@ -15,7 +15,7 @@ from tf.transformations import euler_from_quaternion, quaternion_from_euler
 state = False
 
 
-class Buffer_2():
+class Buffer():
 
     def __init__(self):
 
@@ -48,8 +48,8 @@ class Buffer_2():
 
     def callback_odom(self,var):
 
-        #print(len(self.pointcloud_buffer1.points))
         self.current_odom = var
+
 
     def callback(self,arg):
 
@@ -71,6 +71,7 @@ class Buffer_2():
                 orientation_list = [rot_q.x, rot_q.y, rot_q.z, rot_q.w]
                 (roll, pitch, yaw) = euler_from_quaternion(orientation_list)
 
+
                 self.x      += self.current_odom.pose.pose.position.x
                 self.y      += self.current_odom.pose.pose.position.y
                 self.theta  += yaw
@@ -89,10 +90,11 @@ class Buffer_2():
 
                     #self.remove_duplicates(self.pointcloud_buffer)
                     #self.sampling(self.pointcloud_buffer)
-                    self.x       = self.x/395
-                    self.y       = self.y/395
-                    self.theta   = self.theta
+                    self.x       = self.x/len(self.pointcloud_buffer.points)
+                    self.y       = self.y/len(self.pointcloud_buffer.points)
+                    self.theta   = self.theta/len(self.pointcloud_buffer.points)
 
+                    
 
                     self.final_odom = self.current_odom
 
@@ -161,16 +163,21 @@ class Buffer_2():
             del PointCloud.points[i]
 
 
+
+
 def callback(msg):
     global state
     state = msg.data
+
+
+
 
 
 if __name__ == '__main__':
 
 
 
-    rospy.init_node('Buffer_2', anonymous=True)
+    rospy.init_node('Buffer', anonymous=True)
     initialization = True
 
     while not rospy.is_shutdown():
@@ -185,6 +192,6 @@ if __name__ == '__main__':
 
         elif state == True:
             if initialization == True:
-                buffer = Buffer_2()
-                print "buffer 2 created"
+                buffer = Buffer()
+                print "buffer created"
                 initialization = False

src/icp/static/buffers/mapping.py
@@ -83,11 +83,10 @@ class Mapping():
 
 
         self.sub_sonar                = rospy.Subscriber("/own/simulated/dynamic/sonar_PC"  , PointCloud, self.callback)
-        self.pub_buffer               = rospy.Publisher("/SLAM/buffer/pointcloud_source"    , PointCloud, queue_size = 1)
-        self.pub_map                  = rospy.Publisher("/SLAM/map"                         , PointCloud, queue_size = 1)
         self.sub_odom                 = rospy.Subscriber("/odom"                            , Odometry  , self.callback_odom)
-        self.pub_odom                 = rospy.Publisher("/SLAM/buffer/odom_source"          , Odometry  , queue_size = 1)
 
+        self.pub_odom                 = rospy.Publisher("/SLAM/buffer/odom_source"          , Odometry  , queue_size = 1)
+        self.pub_map                  = rospy.Publisher("/SLAM/map"                         , PointCloud, queue_size = 1)
 
 
     def update(self,pointcloud,T):
@@ -112,14 +111,14 @@ class Mapping():
                 pointcloud.points[i].y = point[1,0]
                 pointcloud.points[i].z = 0
 
-                #self.pointcloud_buffer.points.append(pointcloud.points[i])
+                self.map.points.append(pointcloud.points[i])
 
 
 
 
-    def change_origin(self):
+    def change_origin(self,pointcloud,T):
 
-        T = np.eye(3)
+
         tmp = Point32()
 
         rot_q  = self.final_odom.pose.pose.orientation
@@ -130,16 +129,16 @@ class Mapping():
         x = self.final_odom.pose.pose.position.x
         y = self.final_odom.pose.pose.position.y
 
-        odom = np.array([[x],
-                         [y],
-                         [yaw]])
+        odom = np.array([[x + T[0,2]],
+                         [y + T[1,2]],
+                            [yaw]])
 
 
 
-        for i in range(len(self.map.points)):
+        for i in range(len(pointcloud.points)):
 
             #print(len(self.pointcloud_buffer1.points))
-            tmp_point = self.map.points[i]
+            tmp_point = pointcloud.points[i]
 
 
             tmp_point = from_odom2world(tmp_point,odom)
@@ -155,14 +154,12 @@ class Mapping():
 
     def callback_odom(self,var):
 
-        if self.odom_state == False:
 
-            self.final_odom = var
+        self.final_odom = var
+        self.final_odom.pose.pose.position.x = 0
+        self.final_odom.pose.pose.position.y = 0
 
-            self.final_odom.pose.pose.position.x = -250
-            self.final_odom.pose.pose.position.y = 300
 
-            self.odom_state = True
 
 
     def callback(self,arg):
@@ -203,7 +200,7 @@ class Mapping():
 
 
                 self.map.points.append(point)    # add the new point
-
+                self.pub_odom.publish(self.final_odom)
                 """
                 self.x += self.final_odom.pose.pose.position.x
                 self.y += self.final_odom.pose.pose.position.y
@@ -225,13 +222,10 @@ class Mapping():
                     self.y  = self.y/396
                     """
 
-                    self.final_odom.pose.pose.position.x = -250
-                    self.final_odom.pose.pose.position.y = 300
 
+                    #self.change_origin()
 
-                    self.change_origin()
-
-                    self.pub_buffer.publish(self.map)
+                    self.pub_map.publish(self.map)
                     self.pub_odom.publish(self.final_odom)
 
                     self.sampled = True
@@ -287,6 +281,20 @@ class Mapping():
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def callback_T(msg):
 
 
@@ -317,15 +325,6 @@ def callback(msg):
 
 
 
-def callback_odom_target(msg):
-
-    global target_odom
-
-    odom_target[0,0] = msg.pose.pose.position.x
-    odom_target[1,0] = msg.pose.pose.position.y
-    odom_target[2,0] = 1
-
-
 
 
 if __name__ == '__main__':
@@ -334,7 +333,7 @@ if __name__ == '__main__':
 
     sub1 = rospy.Subscriber('/SLAM/buffer/pointcloud_target', PointCloud, callback_pc)
     sub2 = rospy.Subscriber('/SLAM/T'                       , my_msg    , callback_T)
-    sub3 = rospy.Subscriber('/SLAM/buffer/odom_target'      , Odometry  , callback_odom_target)
+
 
 
     initialization = True
@@ -346,17 +345,15 @@ if __name__ == '__main__':
 
 
 
-
-
         if state == False:
 
-            sub4 = rospy.Subscriber('/SLAM/buffer_1', Bool, callback)
+            sub3 = rospy.Subscriber('/SLAM/buffer_1', Bool, callback)
 
             if initialization == False:
 
                 if update == False:
 
-                    Mapping.update(target_PC,T)
+                    map.change_origin(target_PC,T)
                     update = True
 
 
@@ -366,5 +363,5 @@ if __name__ == '__main__':
 
             if initialization == True:
                 map = Mapping()
-                print "Map created"
+                print "\nMap created\n"
                 initialization = False

src/icp/static/main.py
@@ -98,9 +98,11 @@ def call_buffer_1():
     #Function to erased the previous scan of th buffer 1 and collect a new one
     Timer = rospy.get_time()
     while rospy.get_time() < Timer + 15 :                         # once empty, wait for the buffer to be filled
+                                    # wait for 1 second
         create_buffer_1()                                        # allow the buffer to collect data
+        rospy.sleep(1)
         print"map initialization: ", int(100*(rospy.get_time() - Timer)/15), "%"      # Print the current situation of the buffer
-        rospy.sleep(1)                                           # wait for 1 second
+
 
     return data.return_source()
 
@@ -111,7 +113,7 @@ def call_buffer_2():
     #Function to erased the previous scan of th buffer 2 and collect a new one
 
 
-    raw_input("would you like to process second scan ? [ENTER]") # ask you the permition to execute the second scan
+    raw_input("\nwould you like to process second scan ? [ENTER]\n") # ask you the permition to execute the second scan
 
     while len(pc_target.points) > 2: # while the buffer is not empty
 
@@ -121,9 +123,11 @@ def call_buffer_2():
     #Function to erased the previous scan of th buffer 1 and collect a new one
     Timer = rospy.get_time()
     while rospy.get_time() < Timer + 15 :                         # once empty, wait for the buffer to be filled
+
         create_buffer_2()                                        # allow the buffer to collect data
+        rospy.sleep(1)
         print"scan buffer: ", int(100*(rospy.get_time() - Timer)/15), "%"      # Print the current situation of the buffer
-        rospy.sleep(1)                                           # wait for 1 second
+                                 # wait for 1 second
 
 
     return data.return_target()
@@ -157,7 +161,6 @@ def from_icp2world(odom,point):
 
 
 
-
 def from_world2icp(odom,T):
 
     point = np.zeros((3,1))
@@ -167,9 +170,9 @@ def from_world2icp(odom,T):
     point[2,0] =   1
 
 
-    tmp = np.array([[np.cos(-odom[2,0])  , np.sin(-odom[2,0]) , 0],
+    tmp = np.array([[ np.cos(-odom[2,0]) , np.sin(-odom[2,0]) , 0],
                     [-np.sin(-odom[2,0]) , np.cos(-odom[2,0]) , 0],
-                    [        0          ,             0     , 1],])
+                    [        0           ,          0         , 1],])
 
     point = np.dot(tmp,point)
 
@@ -183,28 +186,31 @@ def from_world2icp(odom,T):
     return T
 
 
+
+
+
 if __name__ == '__main__':
 
     rospy.init_node('Static_SLAM', anonymous=True) 	# initiate the node
-    sub_gt       = rospy.Subscriber('/desistek_saga/pose_gt', Odometry, callback_gt)                   # Subscribes to the Ground Truth pose
+    sub_gt       = rospy.Subscriber('/desistek_saga/pose_gt', Odometry, callback_gt) # Subscribes to the Ground Truth pose
 
-    pub1         = rospy.Publisher('/SLAM/buffer_1', Bool   , queue_size=1)                                    # Create publisher to enable or disable the buffer 1 [True = enable / False = disable]
-    pub2         = rospy.Publisher('/SLAM/buffer_2', Bool   , queue_size=1)                                    # Create publisher to enable or disable the buffer 2 [True = enable / False = disable]
+    pub1         = rospy.Publisher('/SLAM/buffer_1', Bool   , queue_size=1) # Create publisher to enable or disable the buffer 1 [True = enable / False = disable]
+    pub2         = rospy.Publisher('/SLAM/buffer_2', Bool   , queue_size=1) # Create publisher to enable or disable the buffer 2 [True = enable / False = disable]
     pub_odom     = rospy.Publisher('/SLAM/offset'  , my_msg , queue_size=1)
     pub_T        = rospy.Publisher('/SLAM/T'       , my_msg , queue_size=1)
 
     data = retrive_data() # create the class to retrive the data from the scans
     kf = KF()             # create the Kalman Filter
     initialization = True # initialization variable
-    error = 0.0001                                                                       # initiate the counter
+    error = 0.0001        # initiate the counter
 
 
     while not rospy.is_shutdown():
 
 
-
         if initialization == True:
 
+            raw_input("initialization map ?")
             source,odom_source = call_buffer_1() # ask the buffer 1 to scan
 
             target,odom_target = call_buffer_2() # ask the buffer 2 to scan
@@ -213,12 +219,14 @@ if __name__ == '__main__':
 
             target,odom_target = call_buffer_2() # empty the scan 2 and re-ask for scan
 
-        print source
-        print target
-        T = data.initial_guess()   # initial guess of the transform
+        #print source
+        #print target
 
+        T = data.initial_guess()   # initial guess of the transform
+        T = np.eye(3)
         #T = from_world2icp(odom_source,T)
-        print(T)
+
+        #print(T)
         ICP = Align2D(source,target,T)               # create an ICP object
 
         observation,error = ICP.transform                  # get the position according to the ICP
@@ -226,8 +234,8 @@ if __name__ == '__main__':
 
         while error > 0.1:  # if the error is too high
 
-            print "The ICP error is :", error                                                                               # display the error
-            answer = raw_input("would you like to continue or re-scan the environment ([Y] : continue / [N] : re-scan)")    # asking you what to do
+            print "\n The ICP error is :", error, "\n"                                                                               # display the error
+            answer = raw_input("\nwould you like to continue or re-scan the environment ([Y] : continue / [N] : re-scan)\n")    # asking you what to do
 
             if answer == "Y":       # if you want to continue the error becomes 0 and you escape the loop
 
@@ -247,19 +255,21 @@ if __name__ == '__main__':
 
 
 
-        print "Output ICP :\n",observation
+        print "\nOutput ICP :\n",observation
 
         # transform the 3x3 matrix into a 3x1 matrix
+
 
         observation = np.array([[observation[0,2]],                 # x
                                 [observation[1,2]],                 # y
                                 [np.arccos(observation[0,0])]])     # theta
 
+        offset_update = from_icp2world(odom_source,observation)
 
         msg = my_msg()              # create msg type
-        msg.x     = -observation[0,0]   # offset in x
-        msg.y     = -observation[1,0]   # offset in y
-        msg.theta = observation[2,0]   # offset in theta
+        msg.x     = offset_update[0,0]   # offset in x
+        msg.y     = offset_update[1,0]   # offset in y
+        msg.theta = offset_update[2,0]   # offset in theta
         pub_T.publish(msg)       # publish the offset
 
 